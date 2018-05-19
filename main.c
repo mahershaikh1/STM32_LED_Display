a4265bf365772f2edd95fd8c9dcc370e1a2cbe4b
Maher Shaikh <35471992+mahershaikh1@users.noreply.github.com>
2018-05-19 02:46:27 -0400

    code for uart communication between arduino and STM32

--- main.c
+++ main.c
@@ -1,308 +1,208 @@
 /**
   ******************************************************************************
   * @file    main.c
+
   * @author  Ac6
   * @version V1.0
   * @date    01-December-2013
   * @brief   Default main function.
+  * WORKS
   ******************************************************************************
 */
 
-/*
- PSEUDO
+#include <stm32f4xx.h>
+#include <misc.h>			 // I recommend you have a look at these in the ST firmware folder
+#include <stm32f4xx_usart.h> // under Libraries/STM32F4xx_StdPeriph_Driver/inc and src
 
- 1. Clock in data for entire row (use R1,G1,B1,R2,G2,B2 for data and CLK for clock)
+#define MAX_STRLEN 12 // this is the maximum string length of our string in characters
+volatile char received_string[MAX_STRLEN+1]; // this will hold the recieved string
 
-2. OE high
+void Delay(__IO uint32_t nCount) {
+  while(nCount--) {
+  }
+}
 
-3. Select line address (A,B,C,D)
+/* This function initializes the USART1 peripheral
+ *
+ */
+void init_USART2(uint32_t baudrate){
 
-4. LAT high
-
-5. LAT low
-
-6. OE low
-
-*/
-
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-
-
-
-#include "stm32f4xx.h"
-#include "stm32f4xx_rcc.h"
-#include "stm32f4xx_gpio.h"
-
-#include "misc.h"
-#include "delay.h" // http://patrickleyman.be/blog/stm32f407-delay-with-systick/
+	GPIO_InitTypeDef GPIO_InitStruct; // this is for the GPIO pins used as TX and RX
+	USART_InitTypeDef USART_InitStruct; // this is for the USART1 initialization
+	NVIC_InitTypeDef NVIC_InitStructure; // this is used to configure the NVIC (nested vector interrupt controller)
+	//EXTI_InitTypeDef EXTI_InitStruct;
 
 
-#define COLDEPTH 		3
-#define BRIGHTNESS		100
-#define ROWS 			32
-#define COLUMNS 		64
-
-#define R1PORT			GPIOB
-#define R1PIN			GPIO_Pin_9
-#define G1PORT			GPIOD
-#define G1PIN			GPIO_Pin_6
-#define B1PORT			GPIOD
-#define B1PIN			GPIO_Pin_7
-#define R2PORT			GPIOD
-#define R2PIN			GPIO_Pin_4
-#define G2PORT			GPIOD
-#define G2PIN			GPIO_Pin_5
-#define B2PORT			GPIOD
-#define B2PIN			GPIO_Pin_2
-
-#define ROW0PORT		GPIOD
-#define ROW0PIN			GPIO_Pin_3
-#define ROW1PORT		GPIOD
-#define ROW1PIN			GPIO_Pin_0
-#define ROW2PORT		GPIOD
-#define ROW2PIN			GPIO_Pin_1
-#define ROW3PORT		GPIOC
-#define ROW3PIN			GPIO_Pin_11
-
-#define CLKPORT			GPIOC
-#define CLKPIN			GPIO_Pin_12
-#define LEPORT			GPIOA
-#define LEPIN			GPIO_Pin_15
-#define OEPORT			GPIOC
-#define OEPIN			GPIO_Pin_10
+	// IDK
+	//RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
 
 
+	/* enable APB2 peripheral clock for USART1
+	 * note that only USART1 and USART6 are connected to APB2
+	 * the other USARTs are connected to APB1
+	 */
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE); // using USART 2 so APB1
+
+	/* enable the peripheral clock for the pins used by
+	 * USART1, PB6 for TX and PB7 for RX
+	 */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE); // actually using A2(TX) and A3(RX)
+
+	/* This sequence sets up the TX and RX pins
+	 * so they work correctly with the USART1 peripheral
+	 */
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3; // Pins 2 (TX) and 3 (RX) are used
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF; 			// the pins are configured as alternate function so the USART peripheral has access to them
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;		// this defines the IO speed and has nothing to do with the baudrate!
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;			// this defines the output type as push pull mode (as opposed to open drain)
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;			// this activates the pullup resistors on the IO pins
+	GPIO_Init(GPIOA, &GPIO_InitStruct);					// now all the values are passed to the GPIO_Init() function which sets the GPIO registers
+
+	//SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource3);
+
+	/* The RX and TX pins are now connected to their AF
+	 * so that the USART1 can take over control of the
+	 * pins
+	 */
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2); //
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
+
+	/* Now the USART_InitStruct is used to define the
+	 * properties of USART1
+	 */
+	USART_InitStruct.USART_BaudRate = baudrate;				// the baudrate is set to the value we passed into this init function
+	USART_InitStruct.USART_WordLength = USART_WordLength_8b;// we want the data frame size to be 8 bits (standard)
+	USART_InitStruct.USART_StopBits = USART_StopBits_1;		// we want 1 stop bit (standard)
+	USART_InitStruct.USART_Parity = USART_Parity_No;		// we don't want a parity bit (standard)
+	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None; // we don't want flow control (standard)
+	USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx; // we want to enable the transmitter and the receiver
+	USART_Init(USART2, &USART_InitStruct);					// again all the properties are passed to the USART_Init function which takes care of all the bit setting
+
+
+	/* Here the USART2 receive interrupt is enabled
+	 * and the interrupt controller is configured
+	 * to jump to the USART2_IRQHandler() function
+	 * if the USART2 receive interrupt occurs
+	 */
+	//USART_ITConfig(USART2, USART_IT_RXNE, ENABLE); // enable the USART1 receive interrupt
+
+
+//	/* PB12 is connected to EXTI_Line12 */
+//	EXTI_InitStruct.EXTI_Line = EXTI_Line3;
+//	/* Enable interrupt */
+//	EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+//	/* Interrupt mode */
+//	EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+//	/* Triggers on rising and falling edge */
+//	EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
+//	/* Add to EXTI */
+//	EXTI_Init(&EXTI_InitStruct);
+
+
+//	NVIC_InitStructure.NVIC_IRQChannel = EXTI3_IRQn;		 // we want to configure the USART1 interrupts
+//	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;// this sets the priority group of the USART1 interrupts
+//	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;		 // this sets the subpriority inside the group
+//	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			 // the USART1 interrupts are globally enabled
+//	NVIC_Init(&NVIC_InitStructure);							 // the properties are passed to the NVIC_Init function which takes care of the low level stuff
+
+	// finally this enables the complete USART1 peripheral
+	USART_Cmd(USART2, ENABLE);
+
+
+
+
+
+	// LED
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_14 | GPIO_Pin_15; // Led 6 Blue selecte
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT; // Mod out !
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz; // 50 MHZ clock frequenc
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP; // Push pull mod
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP; // Pull up
+	GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+
+}
+
+
+/* This function is used to transmit a string of characters via
+ * the USART specified in USARTx.
+ *
+ * It takes two arguments: USARTx --> can be any of the USARTs e.g. USART1, USART2 etc.
+ * 						   (volatile) char *s is the string you want to send
+ *
+ * Note: The string has to be passed to the function as a pointer because
+ * 		 the compiler doesn't know the 'string' data type. In standard
+ * 		 C a string is just an array of characters
+ *
+ * Note 2: At the moment it takes a volatile char because the received_string variable
+ * 		   declared as volatile char --> otherwise the compiler will spit out warnings
+ * */
+void USART_puts(USART_TypeDef* USARTx, volatile char *s){
+
+	while(*s){
+		// wait until data register is empty
+		while( !(USARTx->SR & 0x00000040) );
+		USART_SendData(USARTx, *s);
+		*s++;
+	}
+}
 
 int main(void) {
 
-	// variables
-	int buffsize;
-	uint8_t *matrixbuff; // my matrix buffer pointer :)
-    int x, y; // coordinates for pixels
-    int i; // for loop thru registers/row
-    int counter = 0;
-    int EN = 1;
-    int rowcounter = 0;
-    int colcounter = 0;
-
-	SysTick_Init(); // used for delay functions...might change later STM tutorials.........gratatata
-	InitMatrixPorts();
-    // GPIO CONFIGURATION
-    // performance optimization / code simplification https://electronics.stackexchange.com/questions/36838/set-stm32-gpio-clock-and-data-pins-as-fast-as-possible
-
-	// using pins D2-D7 as data pins R1G1B1 R2G2B2 u send 32 bits through each because each corresponds to 2/12 LED driver chips of 16-bits
-    // set these pins as OUTPUT
-//	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE); // enable clock
-//    GPIO_InitTypeDef GPIO_InitStructData;
-//    GPIO_InitStructData.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_15 | GPIO_Pin_14;
-//    GPIO_InitStructData.GPIO_Mode = GPIO_Mode_OUT; // Mod out !
-//    GPIO_InitStructData.GPIO_Speed = GPIO_Speed_50MHz; // 50 MHZ clock frequency .. I think this is fine?
-//    GPIO_InitStructData.GPIO_OType = GPIO_OType_PP; // Push pull mod
-//    GPIO_InitStructData.GPIO_PuPd = GPIO_PuPd_DOWN; // Pull up .. I think this is fine?
-//    GPIO_Init(GPIOD, &GPIO_InitStructData); // start it upppp
-//
-//    // using pins E4-E10 as control pins A B C D LAT CLK OE
-//    // set these pins as OUTPUT
-//    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
-//    GPIO_InitTypeDef GPIO_InitStructControl;
-//    GPIO_InitStructControl.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10;
-//    GPIO_InitStructControl.GPIO_Mode = GPIO_Mode_OUT;
-//    GPIO_InitStructControl.GPIO_Speed = GPIO_Low_Speed;
-//    GPIO_InitStructControl.GPIO_OType = GPIO_OType_PP;
-//    GPIO_InitStructControl.GPIO_PuPd = GPIO_PuPd_DOWN;
-//    GPIO_Init(GPIOE, &GPIO_InitStructControl);
-
-    // set initial pin values D2-D7 -> 0 and E4-E9 -> 0 and E10 -> 1
-    GPIO_ResetBits(R1PORT, R1PIN); // R1
-    GPIO_ResetBits(G1PORT, G1PIN); // G1
-    GPIO_ResetBits(B1PORT, B1PIN); // B1
-    GPIO_ResetBits(R2PORT, R2PIN); // R2
-    GPIO_ResetBits(G2PORT, G2PIN); // G2
-    GPIO_ResetBits(B2PORT, B2PIN); // B2
-
-    GPIO_ResetBits(ROW0PORT, ROW0PIN); // A
-    GPIO_ResetBits(ROW1PORT, ROW1PIN); // B
-    GPIO_ResetBits(ROW2PORT, ROW2PIN); // C
-    GPIO_ResetBits(ROW3PORT, ROW3PIN); // D
-
-    GPIO_ResetBits(LEPORT, LEPIN); //
-    GPIO_ResetBits(OEPORT, OEPIN); //
-    GPIO_ResetBits(CLKPORT, CLKPIN); // display is disabled
-
-    // delay_nms(20000);
-
-    // create buffer
-//    buffsize = 32*8*3; // not sure apparently x3 = 3 bytes holds 4 planes packed...?
-//    matrixbuff = (uint8_t*)calloc(buffsize, sizeof(uint8_t));//allocate for single buffer calloc initializes to 0!!! ha
-//    if(matrixbuff == NULL) // error handling
-//    {
-//    	printf("Error! memory not allocated.");
-//        exit(0);
-//    }
+  init_USART2(9600); // initialize USART1 @ 9600 baud
 
 
+  USART_puts(USART2, "A"); // just send a message to indicate that it works
+  int ch;
 
-    while(1)
-    {
-        GPIO_ResetBits(R1PORT, R1PIN); // R1
-        GPIO_ResetBits(G1PORT, G1PIN); // G1
-        GPIO_ResetBits(B1PORT, B1PIN); // B1
-        GPIO_ResetBits(R2PORT, R2PIN); // R2
-        GPIO_ResetBits(G2PORT, G2PIN); // G2
-        GPIO_ResetBits(B2PORT, B2PIN); // B2
-
-//        for (rowcounter=0 ; rowcounter<(ROWS/2) ; rowcounter++)
-//        {
-//        	GPIO_SetBits(OEPORT,OEPIN);
-//        	for (colcounter=0 ; colcounter<COLUMNS ; colcounter++)
-//        	{
-//        		GPIO_ResetBits(CLKPORT,CLKPIN);
-//
-//        		GPIO_SetBits(R1PORT,R1PIN);
-//        		GPIO_SetBits(G1PORT,G1PIN);
-//        		GPIO_SetBits(B1PORT,B1PIN);
-//
-//        		GPIO_SetBits(CLKPORT,CLKPIN);
-//        		//if (colcounter>brightvalue) GPIO_ResetBits(OEPORT,OEPIN);
-//        	}
-//
-//        	GPIO_SetBits(OEPORT,OEPIN);
-//        	delay_nms(1);
-//        	GPIO_SetBits(LEPORT,LEPIN);
-//        	if ((rowcounter & 0x01)==0x01) GPIO_SetBits(ROW0PORT,ROW0PIN); else GPIO_ResetBits(ROW0PORT,ROW0PIN);
-//        	if ((rowcounter & 0x02)==0x02) GPIO_SetBits(ROW1PORT,ROW1PIN); else GPIO_ResetBits(ROW1PORT,ROW1PIN);
-//        	if ((rowcounter & 0x04)==0x04) GPIO_SetBits(ROW2PORT,ROW2PIN); else GPIO_ResetBits(ROW2PORT,ROW2PIN);
-//        	if ((rowcounter & 0x08)==0x08) GPIO_SetBits(ROW3PORT,ROW3PIN); else GPIO_ResetBits(ROW3PORT,ROW3PIN);
-//        	GPIO_ResetBits(LEPORT,LEPIN);
-//        	//delay_nms(delaybrightness);
-//        	delay_nms(1);
-//        	GPIO_ResetBits(OEPORT,OEPIN);
-//        	//delay_nms(59*(pwmindex-(8-COLDEPTH)+1)*(pwmindex-(8-COLDEPTH)+1)-59);
-//        }
-
-        for (rowcounter=0 ; rowcounter<(ROWS/2) ; rowcounter++)
-        {
-
-			GPIO_SetBits(OEPORT, OEPIN);
-
-			// SHIFT IN
-			for(i=0; i<64; i++)
-			{
-				GPIO_ResetBits(CLKPORT, CLKPIN);
-
-				delay_nus(100);
-
-				GPIO_SetBits(R1PORT, R1PIN);
-				GPIO_SetBits(R2PORT, R2PIN);
-
-				delay_nus(100);
-
-				GPIO_SetBits(CLKPORT, CLKPIN);
-
-			}
-
-			// LATCH / ENABLE
-			GPIO_SetBits(OEPORT, OEPIN);
-
-			//delay_nus(100);
-
-			GPIO_SetBits(LEPORT,LEPIN);
-			if ((rowcounter & 0x01)==0x01) GPIO_SetBits(ROW0PORT,ROW0PIN); else GPIO_ResetBits(ROW0PORT,ROW0PIN);
-			if ((rowcounter & 0x02)==0x02) GPIO_SetBits(ROW1PORT,ROW1PIN); else GPIO_ResetBits(ROW1PORT,ROW1PIN);
-			if ((rowcounter & 0x04)==0x04) GPIO_SetBits(ROW2PORT,ROW2PIN); else GPIO_ResetBits(ROW2PORT,ROW2PIN);
-			if ((rowcounter & 0x08)==0x08) GPIO_SetBits(ROW3PORT,ROW3PIN); else GPIO_ResetBits(ROW3PORT,ROW3PIN);
-			GPIO_ResetBits(LEPORT, LEPIN);
-
-			//delay_nus(100);
-
-			//delay_nus(1000);
-
-			GPIO_ResetBits(OEPORT, OEPIN);
-
-			delay_nms(100);
-
-			//delay_nus(1000);
-
-        }
-    }
+  while (1)
+  {
+	  //USART2_IRQHandler();
+	  //GPIO_SetBits(GPIOD, GPIO_Pin_14);
+	  //GPIO_ResetBits(GPIOD, GPIO_Pin_14);
 
 
+	    if (USART_GetFlagStatus(USART2, USART_FLAG_RXNE) != RESET)
+	    {
+	    	ch = USART_ReceiveData(USART2);
+	    	ch = USART_ReceiveData(USART2);
+	    	ch = USART_ReceiveData(USART2);
+	    	//printf("%c", ch&0xff);
 
+	    	GPIO_SetBits(GPIOD, GPIO_Pin_15);
+	    	break;
+		}
+  }
 
-
-
-
-    //GPIO_ResetBits(GPIOE, GPIO_Pin_10); // OE low -> turns it on
-
-
-
-
-
-
-    // memset(matrixbuff, 0, buffsize);
-
-
-//    if (x < 0 || x>31 || y < 0 || y>15) // error handling
-//    {
-//    	printf("Error! Pixel is off the grid");
-//        exit(0);
-//    }
-
-
-
-    // ISR??????? FOR TIMER???? HOW???? -> REFRESH DISPLAAAYAAY
-
-
-
-//    free(matrixbuff); // free memory
-
-
-
-
-
-
+  USART_puts(USART2, ch);
 }
 
+// this is the interrupt request 	 (IRQ) for ALL USART1 interrupts
+void USART2_IRQHandler(void){
 
-void InitMatrixPorts() {
-	GPIO_InitTypeDef      GPIO_InitStructure;
+	GPIO_SetBits(GPIOD, GPIO_Pin_14); // Toggle the LED, so you can see it change as you step each function
+	//GPIO_ResetBits(GPIOD, GPIO_Pin_14);
+	// check if the USART1 receive interrupt flag was set
+	if( USART_GetITStatus(USART2, USART_IT_RXNE)){
 
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOC | RCC_AHB1Periph_GPIOD | RCC_AHB1Periph_GPIOG, ENABLE);
+		GPIO_SetBits(GPIOD, GPIO_Pin_15); // Toggle the LED, so you can see it change as you step each function
+		//GPIO_ResetBits(GPIOD, GPIO_Pin_15);
 
-  	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
-	GPIO_InitStructure.GPIO_Speed = GPIO_High_Speed;
-	GPIO_InitStructure.GPIO_Pin = R1PIN;
-  	GPIO_Init(R1PORT, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = G1PIN;
-  	GPIO_Init(G1PORT, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = B1PIN;
-  	GPIO_Init(B1PORT, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = R2PIN;
-  	GPIO_Init(R2PORT, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = G2PIN;
-  	GPIO_Init(G2PORT, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = ROW3PIN;
-  	GPIO_Init(ROW3PORT, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = ROW2PIN;
-  	GPIO_Init(ROW2PORT, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = ROW1PIN;
-  	GPIO_Init(ROW1PORT, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = ROW0PIN;
-  	GPIO_Init(ROW0PORT, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = B2PIN;
-  	GPIO_Init(B2PORT, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = OEPIN;
-  	GPIO_Init(OEPORT, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = LEPIN;
-  	GPIO_Init(LEPORT, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = CLKPIN;
-  	GPIO_Init(CLKPORT, &GPIO_InitStructure);
-}
+		static uint8_t cnt = 0; // this counter is used to determine the string length
+		char t = USART2->DR; // the character from the USART1 data register is saved in t
 
-void SysTick_Handler(void) {
-	TimeTick_Decrement();
+		/* check if the received character is not the LF character (used to determine end of string)
+		 * or the if the maximum string length has been been reached
+		 */
+		if( (t != '\n') && (cnt < MAX_STRLEN) ){
+			received_string[cnt] = t;
+			cnt++;
+		}
+		else{ // otherwise reset the character counter and print the received string
+			cnt = 0;
+			USART_puts(USART2, received_string);
+		}
+	}
 }
